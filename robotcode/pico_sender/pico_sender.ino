b1cbc8baeebff40aae5a8968776452cad8dba499
BrianKatz925 <54519920+BrianKatz925@users.noreply.github.com>
2022-03-01 11:11:24 -0500

    pico_sender reorganized - moving onto sami non reverse

--- robotcode/pico_sender/pico_sender.ino
+++ robotcode/pico_sender/pico_sender.ino
@@ -1,44 +1,51 @@
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp32-esp8266/
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
 
 /**
-   UPLOAD THIS CODE TO THE SENDER
+   Uploaded to external ESP Sender - Handles WiFi communication to Mainboard and prints along Serial for debugging
 */
 
 #include <esp_now.h>
 #include <WiFi.h>
 
-/*
-    REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
-    ADD OR DELETE LINES DEPENDING ON NUMBER OF RECEIVER BOARDS
-*/
-uint8_t broadcastAddress1[] = {0x24, 0xA1, 0x60, 0x75, 0xB8, 0xE0};
+/**********************************************************************************************************************
 
-String deviceBData = "";
+                                                    VARIABLES + DEFINITIONS
 
 
-//type struct with two integer variables
-typedef struct data_struct_rec {
+ **********************************************************************************************************************/
+//ESP-NOW Variables
+uint8_t broadcastAddress1[] = {0x24, 0xA1, 0x60, 0x75, 0xB8, 0xE0}; //replace with the MAC Address of your ESP
+
+
+typedef struct data_struct_rec { //data struct to receive wifi commands from the external ESP  - must match data struct sent from mainboard
   String wifiData;
 } data_struct_rec;
 
-typedef struct data_struct {
+typedef struct data_struct { //data struct to receive from the mainboard containing address, current sensor, and encoder data
+  // data types must match data struct that mainboard is expecting
   int smdAddress;
   int currentData;
   int encoderData;
 } data_struct;
 
-data_struct_rec test; //store variable values
-data_struct receiveData;
+data_struct_rec test; //create instance of sending struct to be populated with data
+data_struct receiveData; //create instance of receiving struct 
 
+String deviceBData = ""; //variable for inputting commands into the serial monitor and storing the input
+
+String newData = "";
+
+char deviceBdata;
+char input[43];
+
+
+/**********************************************************************************************************************
+
+                                                         SETUP + LOOP
+
+
+ **********************************************************************************************************************/
+ 
 void setup() {
   Serial.begin(9600);
 
@@ -62,6 +69,7 @@ void setup() {
   esp_now_peer_info_t peerInfo;
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
+  
   // register first peer
   memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
@@ -69,10 +77,6 @@ void setup() {
     return;
   }
 }
-
-String newData = "";
-
-
 
 void loop() {
   if (Serial.available() > 0) {
@@ -105,11 +109,69 @@ void loop() {
 }
 
 
-char deviceBdata;
-char input[43];
+/**********************************************************************************************************************
+
+                                                    ESP-NOW WIFI HELPER FUNCTIONS
+
+
+ **********************************************************************************************************************/
+
+/**
+    Callback function to be executed when WiFi data is sent to mainboard
+    prints if message was successfully delivered to know if board received message
+    @param mac_addr - the mac address of the board that data is being sent to
+    @param status - the status of the transaction - success or fail
+*/
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  //Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  // Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+
+/**
+   Callback function to be executed when WiFi data is received from mainboard
+   Prints received smart motor driver address, current sensor and encoder data
+   @param mac - the mac address of the board sending the data
+   @param incomingData - the data to be copied into the myData variable - the instance of the receiving struct
+   @param len the number of bytes received
+*/
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&receiveData, incomingData, sizeof(receiveData)); //copy content of incomingdata variable into mydata variable
+
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Smart Motor Driver Address: ");
+  Serial.println(receiveData.smdAddress);
+  Serial.print("Current Sensor Reading: ");
+  Serial.println(receiveData.currentData);
+  Serial.print("Encoder Reading: ");
+  Serial.println(receiveData.encoderData);
+  Serial.println("");
+}
+
+
+/**********************************************************************************************************************
+
+                                                    CONTROLLER FEEDBACK FUNCTIONS
+
+
+ **********************************************************************************************************************/
+
+
+/**
+ * Function to interpret input from Logitech gamepad from registers and return a command as a String to send to the mainboard
+ * @param data - String output from controller 
+ */
 String interpretData(String data) {
   data.toCharArray(input, 43);
   //Serial.println(data);
+  
   //A button is index 0
 
   //make D pad into integer 0-7
@@ -119,20 +181,23 @@ String interpretData(String data) {
     dpadval *= 2;
     if (data[i] == '1') dpadval++;
   }
+  
   //check buttons
-  if (data[5] == '1') { //x button -send data
+  if (data[5] == '1') { //x button - send data
     return "2";
   }
 
-  if (data[8] == '1' && data[7] == '1') { //left bumper and down button
-    return "23"; //run the lead screw till current dunks on it
+  if (data[8] == '1' && data[7] == '1') { //left bumper and down button - run the lead screw till current dunks on it
+    return "23"; 
   }
-  if (data[8] == '1' && data[4] == '1') { //left bumper and down button
-    return "24"; //run the lead screw till current dunks on it
+  if (data[8] == '1' && data[4] == '1') { //left bumper and down button - run the lead screw till current dunks on it
+    return "24"; 
   }
   if (data[4] == '1') { //y button - lead screw up
     return "1";
   }
+
+  //second profile 
   if (data[6] == '1' && dpadval == 2) { //B button with dpad
     return "18";
   }
@@ -206,33 +271,4 @@ String interpretData(String data) {
   //Serial.println(rjoyval);
   //Serial.print("dpad: ");
   //Serial.println(dpad);
-}
-
-// callback when data is sent
-// executed when data is sent, prints if message was successfully delivered to know if board received message
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  char macStr[18];
-  //Serial.print("Packet to: ");
-  // Copies the sender mac address to a string
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  // Serial.print(macStr);
-  Serial.print(" send status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
-
-
-//callback function that will be executed when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&receiveData, incomingData, sizeof(receiveData)); //copy content of incomingdata variable into mydata variable
-
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("Smart Motor Driver Address: ");
-  Serial.println(receiveData.smdAddress);
-  Serial.print("Current Sensor Reading: ");
-  Serial.println(receiveData.currentData);
-  Serial.print("Encoder Reading: ");
-  Serial.println(receiveData.encoderData);
-  Serial.println("");
 }
